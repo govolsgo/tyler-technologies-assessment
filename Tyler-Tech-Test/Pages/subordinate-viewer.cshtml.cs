ee43339b5fb76d8cd1250c67fb6fc21748e551f4
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-19 05:46:27 -0500

    Moving code from the raw HTML page subordinate-viewer.html to new subordinate-viewer.cshtml.

---
+++ Tyler-Tech-Test/Pages/subordinate-viewer.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tyler_Tech_Test.Pages
+{
+	public class subordinate_viewerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
