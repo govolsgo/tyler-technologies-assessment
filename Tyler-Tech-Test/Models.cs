3d80e0a2fe00e55de9b8e1b070302c485c7118cc
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-21 11:38:23 -0500

    Adding new class, Employee, to Models.cs.

--- Tyler-Tech-Test/Models.cs
+++ Tyler-Tech-Test/Models.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 namespace Tyler_Tech_Test
 {
-	public class Models
-	{
-		public Models()
-		{
-		}
-	}
+    public class Employee
+    {
+        public Guid ID;
+        public string EmployeeID;
+        public string FirstName;
+        public string LastName;
+        public Guid Manager;
+        public List<string> Roles;
+    }
 }
 
