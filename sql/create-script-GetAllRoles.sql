5b4e37afb0dfa01268b3110e31181f96e9610edd
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-23 01:46:40 -0500

    Fixing SQL in procedure [GetAllRoles].

--- sql/create-script-GetAllRoles.sql
+++ sql/create-script-GetAllRoles.sql
@@ -1,16 +1,18 @@
 USE [S-Squared-Enterprises]
 
-IF NOT EXISTS
+IF EXISTS
 (
     SELECT 1
     FROM [INFORMATION_SCHEMA].[ROUTINES] WITH (NOLOCK)
     WHERE [ROUTINE_NAME] = 'GetAllRoles'
     AND [ROUTINE_TYPE] = 'PROCEDURE'
 )
-
 BEGIN
-    CREATE PROCEDURE [dbo].[GetAllRoles]
-    AS
-        SELECT *
-        FROM [dbo].[Roles]
+	DROP PROCEDURE [GetAllRoles]
 END
+GO
+
+CREATE PROCEDURE [dbo].[GetAllRoles]
+AS
+SELECT *
+FROM [dbo].[Roles]
