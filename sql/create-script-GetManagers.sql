39fb26cb08baaaa34fe3ac68656d82127bf4a030
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-23 01:49:32 -0500

    Fixing SQL in procedure [GetManagers].

--- sql/create-script-GetManagers.sql
+++ sql/create-script-GetManagers.sql
@@ -1,22 +1,23 @@
 USE [S-Squared-Enterprises]
 
-IF NOT EXISTS
+IF EXISTS
 (
     SELECT 1
     FROM [INFORMATION_SCHEMA].[ROUTINES] WITH (NOLOCK)
     WHERE [ROUTINE_NAME] = 'GetManagers'
     AND [ROUTINE_TYPE] = 'PROCEDURE'
 )
-
 BEGIN
-    CREATE PROCEDURE [dbo].[GetManagers]
-    AS
-        SELECT DISTINCT [Manager]
-        INTO #ManagerGuidList
-        FROM [dbo].[Employees]
-
-        SELECT *
-        FROM [dbo].[Employees] [e]
-        INNER JOIN [#ManagerGuidList] [mgl]
-        ON [mgl].[Manager] = [e].[ID]
+	DROP PROCEDURE [GetManagers]
 END
+
+CREATE PROCEDURE [dbo].[GetManagers]
+AS
+SELECT DISTINCT [Manager]
+INTO #ManagerGuidList
+FROM [dbo].[Employees]
+
+SELECT *
+FROM [dbo].[Employees] [e]
+INNER JOIN [#ManagerGuidList] [mgl]
+ON [mgl].[Manager] = [e].[ID]
