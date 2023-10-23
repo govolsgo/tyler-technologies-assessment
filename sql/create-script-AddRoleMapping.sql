a4c5179e73a529390569445a4b493842032a09d7
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-23 01:39:59 -0500

    Fixing SQL in script [AddRoleMapping].

--- sql/create-script-AddRoleMapping.sql
+++ sql/create-script-AddRoleMapping.sql
@@ -1,31 +1,33 @@
 USE [S-Squared-Enterprises]
 
-IF NOT EXISTS
+IF EXISTS
 (
     SELECT 1
     FROM [INFORMATION_SCHEMA].[ROUTINES] WITH (NOLOCK)
     WHERE [ROUTINE_NAME] = 'AddRoleMapping'
     AND [ROUTINE_TYPE] = 'PROCEDURE'
 )
-
 BEGIN
-    CREATE PROCEDURE [dbo].[AddRoleMapping]
-    (
-        @ID UNIQUEIDENTIFIER,
-        @Employee UNIQUEIDENTIFIER,
-        @Role UNIQUEIDENTIFIER
-    )
-    AS
-        INSERT INTO [dbo].[RoleMappings]
-        (
-            ID,
-            UserID,
-            Role
-        )
-        VALUES
-        (
-            @ID,
-            @Employee,
-            @Role
-        )
+	DROP PROCEDURE [AddRoleMapping]
 END
+GO
+
+CREATE PROCEDURE [dbo].[AddRoleMapping]
+(
+    @ID UNIQUEIDENTIFIER,
+    @Employee UNIQUEIDENTIFIER,
+    @Role UNIQUEIDENTIFIER
+)
+AS
+INSERT INTO [dbo].[RoleMappings]
+(
+    ID,
+    UserID,
+    RoleID
+)
+VALUES
+(
+    @ID,
+    @Employee,
+    @Role
+)
