f84db3bc866a66f00b41007f307fcb4ac50da8f8
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-23 01:48:10 -0500

    Fixing SQL in procedure [GetEmployeesByManager].

--- sql/create-script-GetEmployeesByManager.sql
+++ sql/create-script-GetEmployeesByManager.sql
@@ -1,20 +1,22 @@
 USE [S-Squared-Enterprises]
 
-IF NOT EXISTS
+IF EXISTS
 (
     SELECT 1
     FROM [INFORMATION_SCHEMA].[ROUTINES] WITH (NOLOCK)
     WHERE [ROUTINE_NAME] = 'GetEmployeesByManager'
     AND [ROUTINE_TYPE] = 'PROCEDURE'
 )
-
 BEGIN
-    CREATE PROCEDURE [dbo].[GetEmployeesByManager]
-    (
-        @Manager UNIQUEIDENTIFIER
-    )
-    AS
-        SELECT *
-        FROM [dbo].[Employees]
-        WHERE [Manager] = @Manager
+	DROP PROCEDURE [GetEmployeesByManager]
 END
+GO
+
+CREATE PROCEDURE [dbo].[GetEmployeesByManager]
+(
+    @Manager UNIQUEIDENTIFIER
+)
+AS
+SELECT *
+FROM [dbo].[Employees]
+WHERE [Manager] = @Manager
