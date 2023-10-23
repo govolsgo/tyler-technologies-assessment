5424ad54bbff4b76ff110fb7d4a448bbfb03d561
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-23 01:50:50 -0500

    Fixing SQL in procedure [GetRolesByEmployee].

--- sql/create-script-GetRolesByEmployee.sql
+++ sql/create-script-GetRolesByEmployee.sql
@@ -1,20 +1,22 @@
 USE [S-Squared-Enterprises]
 
-IF NOT EXISTS
+IF EXISTS
 (
     SELECT 1
     FROM [INFORMATION_SCHEMA].[ROUTINES] WITH (NOLOCK)
     WHERE [ROUTINE_NAME] = 'GetRolesByEmployee'
     AND [ROUTINE_TYPE] = 'PROCEDURE'
 )
-
 BEGIN
-    CREATE PROCEDURE [dbo].[GetRolesByEmployee]
-    (
-        @EmployeeGUID UNIQUEIDENTIFIER
-    )
-    AS
-        SELECT *
-        FROM [dbo].[RoleMappings]
-        WHERE [UserID] = @EmployeeGUID
+	DROP PROCEDURE [GetRolesByEmployee]
 END
+GO
+
+CREATE PROCEDURE [dbo].[GetRolesByEmployee]
+(
+    @EmployeeGUID UNIQUEIDENTIFIER
+)
+AS
+SELECT *
+FROM [dbo].[RoleMappings]
+WHERE [UserID] = @EmployeeGUID
