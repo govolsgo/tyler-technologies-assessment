0eb3b0900057e506786aa800ddd2358ee0b96540
Carter Crews <govolsgo@users.noreply.github.com>
2023-10-22 23:31:02 -0500

    Adding code to file create-script-AddEmployee.sql.

---
+++ sql/create-script-AddEmployee.sql
@@ -0,0 +1,37 @@
+USE [S-Squared-Enterprises]
+
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM [INFORMATION_SCHEMA].[ROUTINES] WITH (NOLOCK)
+    WHERE [ROUTINE_NAME] = 'AddEmployee'
+    AND [ROUTINE_TYPE] = 'PROCEDURE'
+)
+
+BEGIN
+    CREATE PROCEDURE [dbo].[AddEmployee]
+    (
+        @ID UNIQUEIDENTIFIER,
+        @Manager UNIQUEIDENTIFIER = NULL,
+        @EmployeeID VARCHAR(100),
+        @FirstName VARCHAR(100),
+        @LastName VARCHAR(100)
+    )
+    AS
+        INSERT INTO [dbo].[Employees]
+        (
+            ID,
+            Manager,
+            EmployeeID,
+            FirstName,
+            LastName
+        )
+        VALUES
+        (
+            @ID,
+            @Manager,
+            @EmployeeID,
+            @FirstName,
+            @LastName
+        )
+END
